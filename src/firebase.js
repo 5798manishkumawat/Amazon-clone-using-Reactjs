e97bd40e9cc7f372b1ef291a5cc86a0308622597
5798manishkumawat <5798manishkumawat@gmail.com>
2020-11-10 12:17:24 +0530

    Setup done

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: "AIzaSyD_vh_F_nGqC4Sf7srwUsiclihbWg9xMrk",
+	authDomain: "clone-using-reactjs.firebaseapp.com",
+	databaseURL: "https://clone-using-reactjs.firebaseio.com",
+	projectId: "clone-using-reactjs",
+	storageBucket: "clone-using-reactjs.appspot.com",
+	messagingSenderId: "649013658606",
+	appId: "1:649013658606:web:88cb3a7775a041926784ff",
+	measurementId: "G-SQ6MHR4VXH",
+};
